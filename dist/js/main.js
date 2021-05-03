afc8355ffe9faf63107054dbc794c8bc6312d6a2
Mulaza <mulazajacinto@Gmail.com>
2021-05-03 11:36:53 +0800

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+const activeArea = document.querySelector("[data-tags='active']");
+const text = activeArea.innerHTML;
+
+let regex = new RegExp(/\#(\w+)/g);
+let matches = text.match(regex);
+console.log("Matches:", matches);
+
+// clear area
+activeArea.innerHTML = '';
+
+matches.forEach((i) => {
+
+    let node = document.createElement('i');
+
+    node.innerHTML = i.replace('#', '')
+    node.classList.add('tag');
+    activeArea.appendChild(node);
+});
+
