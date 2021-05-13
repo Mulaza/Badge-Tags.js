ef3f30bad57c74cc8519cc68a8e07ba59a6eba19
Mulaza <mulazajacinto@Gmail.com>
2021-05-13 16:42:56 +0800

    updated'

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,25 +1,49 @@
 
 
-(function () {
-
-    const activeArea = document.querySelector("[data-tags='active']");
-    const text = activeArea.innerHTML;
-
-    let regex = new RegExp(/\#(\w+)/g);
-    let matches = text.match(regex);
-    console.log("Matches:", matches);
-
-    // clear area
-    activeArea.innerHTML = '';
-
-    matches.forEach((i) => {
-
-        let node = document.createElement('i');
-
-        node.innerHTML = i.replace('#', '')
-        node.classList.add('tag');
-        activeArea.appendChild(node);
-    });
+//
+class Tags {
+    constructor(callback = undefined) {
+        this.callback = callback
+        this.autorun = this.autorun()
+    }
 
 
-})()
+    autorun() {
+
+        const activeArea = document.querySelector("[data-tags='active']");
+        const text = activeArea.innerHTML;
+
+        let regex = new RegExp(/\#(\w+)/g);
+        let matches = text.match(regex);
+
+        // clear area
+        activeArea.innerHTML = '';
+
+        matches.forEach((i) => {
+
+            let node = document.createElement('i');
+
+            node.innerHTML = i.replace('#', '');
+            node.classList.add('tag');
+
+
+            node.addEventListener('click', this.callback)
+
+
+
+            activeArea.appendChild(node);
+        });
+
+    }
+
+
+}
+
+
+function dd(event) {
+    console.log(event.target.innerHTML, "meek")
+}
+
+// Takes one callback
+let vvs = new Tags(dd);
+
